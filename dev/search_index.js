363c52adbab8c2d2f719445a5de2fb4ac874ffe3
Documenter.jl <documenter@juliadocs.github.io>
2025-06-29 07:10:22 +0000

    build based on 4b5aed4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GeneralizedRFF","page":"Home","title":"GeneralizedRFF","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeneralizedRFF.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
